e4fc3e1df27c1ba98e13fe7cbfc417126197f23d
Bartosz Spiżarny <64313992+Bartolomeo26@users.noreply.github.com>
2023-11-05 00:59:14 +0100

    Add files

---
+++ zad4/zad4/zadanie4/zadanie4/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  zadanie4
+//
+//  Created by Bartosz Spizarny on 04/11/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
